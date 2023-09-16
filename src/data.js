b168627edba33f979f1b152f3bab6a2c816db88a
yashdadhich <98592915+yashdadhich@users.noreply.github.com>
2023-09-16 17:07:43 +0530

    code updated

---
+++ src/data.js
@@ -0,0 +1,42 @@
+export const data = [
+    {
+      id: 1,
+      name: "Yash",
+    },
+    {
+      id: 2,
+      name: "Dadhich",
+    },
+    {
+      id: 3,
+      name: "Abhi",
+    },
+    {
+      id: 4,
+      name: "prateek",
+    },
+    {
+      id: 5,
+      name: "shry",
+    },
+    {
+      id: 6,
+      name: "shubham",
+    },
+    {
+      id: 7,
+      name: "lalit",
+    },
+    {
+      id: 8,
+      name: "garvit",
+    },
+    {
+      id: 9,
+      name: "neel",
+    },
+    {
+      id: 10,
+      name: "vansahj",
+    },
+  ];
